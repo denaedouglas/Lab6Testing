aa668881a2302dd1cf50d3ae764191848985193a
denaedouglas <d102d638@ku.edu>
2016-11-08 22:19:29 -0600

    Committing all files for lab 6

---
+++ Lab6/eecs448-lab06-base/Test.cpp
@@ -0,0 +1,13 @@
+#include "Test.h"
+#include <string>
+#include <iostream>
+
+LinkedListOfInts ls;
+
+Test::Test(){
+
+}
+
+void Test::runTests(){
+  std::cout << "Hello";
+}
---
+++ Lab6/eecs448-lab06-base/Test.h
@@ -0,0 +1,9 @@
+#ifndef Test_H
+#define Test_H
+
+class Test{
+public:
+  Test();
+  void runTests();
+};
+#endif
